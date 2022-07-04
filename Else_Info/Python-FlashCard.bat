0fc7033970fbbec3b5d9045163f36eaa6c76fcde
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-04 13:44:40 +0300

    Update

---
+++ Else_Info/Python-FlashCard.bat
@@ -0,0 +1,9 @@
+@echo off
+SET PATH=%CD%\..\..\GIT-Github\Programs\Portable-Python-3.8.9-x64\App\Python\;%PATH%
+SET PATH=%CD%\..\..\GIT-Github\Programs\Portable-Python-3.8.9-x64\App\Python\Scripts\;%PATH%
+SET PATH=%CD%\..\..\GIT-Github\Programs\Portable-Python-3.8.9-x64\App\Python\;%PATH%
+SET PATH=%CD%\..\..\GIT-Github\Programs\gnupg-portable-win32-2.2.27-10\bin\;%PATH%
+SET WORKON_HOME=%CD%\..\env-x64\
+DOSKEY clear=cls
+cd ..\
+cmd.exe
