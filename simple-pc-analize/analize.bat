bd2f4735c190db81d990a5cd10d3b71892c3d8e3
Mikhail Artamonov <maximalis171091@yandex.ru>
2022-07-25 17:19:50 +0300

    Update

---
+++ simple-pc-analize/analize.bat
@@ -0,0 +1,7 @@
+@echo off
+chcp 1252
+SET LOGFILE=%USERNAME%-%date%-log.txt
+SET HOSTIN=%SYSTEMDRIVE%\windows\system32\drivers\etc\hosts
+SET HOSTOUT=%USERNAME%-hosts-%date%.txt
+copy /Y %HOSTIN% %HOSTOUT%
+defrag /A /C > %LOGFILE%
